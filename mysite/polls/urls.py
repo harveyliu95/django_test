c6c7df8b6d76ab290ea8aa50b07bcb711f14ffa6
harveyliu95 <liutongl5@gmail.com>
2015-02-21 16:17:57 +0800

    Tutorial, Part 4, continued
    generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 
 from polls import views
 
+"""
+### Tutorial 3 and before
 
 urlpatterns = patterns('',
     #ex: /polls/
@@ -10,6 +12,19 @@ urlpatterns = patterns('',
     url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
     #ex: /polls/5/results
     url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    #ex: /polls/5/vote
+    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+)
+"""
+
+### tutorial 4, apply generic view
+urlpatterns = patterns('',
+    #ex: /polls/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    #ex: /polls/5/
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    #ex: /polls/5/results
+    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
     #ex: /polls/5/vote
     url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 )
